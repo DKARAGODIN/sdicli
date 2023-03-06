3091d8901a63adf3699dab05095960196e218d45
OldKrab <4shushakov@gmail.com>
2023-03-06 19:03:17 +0300

    implement Parser and tests for it

--- src/main/java/pro/karagodin/commands/Command.java
+++ src/main/java/pro/karagodin/commands/Command.java
@@ -24,4 +24,17 @@ public abstract class Command {
     public void setExitCode(int exitCode) {
         this.exitCode = exitCode;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Command cmd = (Command) o;
+        return cmd.arguments.equals(arguments) && exitCode == cmd.exitCode;
+    }   
+
+    @Override
+    public String toString() {
+        return "command with args: " + arguments.toString();
+    }
 }
