206dd12b72f8e6d20bbb36ce4d6587fabb8dfb0b
OldKrab <4shushakov@gmail.com>
2023-03-13 18:28:31 +0300

    refactor Parser

--- src/main/java/pro/karagodin/Parser.java
+++ src/main/java/pro/karagodin/Parser.java
@@ -2,6 +2,8 @@ package pro.karagodin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 import pro.karagodin.commands.CatCommand;
 import pro.karagodin.commands.Command;
@@ -12,7 +14,18 @@ import pro.karagodin.commands.PwdCommand;
 import pro.karagodin.commands.WcCommand;
 import pro.karagodin.exceptions.CLIException;
 
+import static java.util.Map.entry;
+
 public class Parser {
+
+    private static final Map<String, Function<String, Command>> STR_TO_CMD_FACTORY = Map.ofEntries(
+            entry("cat", c -> new CatCommand()),
+            entry("exec", ExecuteCommand::new),
+            entry("wc", c -> new WcCommand()),
+            entry("pwd", c -> new PwdCommand()),
+            entry("exit", c -> new ExitCommand()),
+            entry("echo", c -> new EchoCommand()));
+
     public List<Command> parse(List<Lexeme> lexemes) throws CLIException {
         var cmdAndArgs = parseCommandAndArguments(lexemes);
         var cmd = getCommandByName(cmdAndArgs.get(0));
@@ -22,39 +35,32 @@ public class Parser {
 
     private List<String> parseCommandAndArguments(List<Lexeme> lexemes) {
         List<String> args = new ArrayList<>();
-        var argBuilder = new StringBuilder();
-        int i = 0;
-        while (i < lexemes.size() && lexemes.get(i).getType() == LexemeType.SPACE) {
-            i++;
-        }
+        int i = skipSpaces(0, lexemes);
         while (i < lexemes.size()) {
-            while (i < lexemes.size() && lexemes.get(i).getType() != LexemeType.SPACE) {
-                argBuilder.append(lexemes.get(i).getView());
-                i++;
-            }
-            args.add(argBuilder.toString());
-            argBuilder.setLength(0);
-            while (i < lexemes.size() && lexemes.get(i).getType() == LexemeType.SPACE) {
-                i++;
-            }
+            var argComps = getArgsComponents(i, lexemes);
+            i += argComps.size();
+            args.add(String.join("", argComps));
+            i = skipSpaces(i, lexemes);
         }
         return args;
     }
 
+
+    private int skipSpaces(int startIndex, List<Lexeme> lexemes) {
+        return (int) (startIndex + lexemes.stream().skip(startIndex).takeWhile(l -> l.getType() == LexemeType.SPACE).count());
+    }
+
+    private List<String> getArgsComponents(int startIndex, List<Lexeme> lexemes) {
+        return lexemes.stream().skip(startIndex).takeWhile(lex -> lex.getType() != LexemeType.SPACE).map(Lexeme::getView).toList();
+    }
+
     private Command getCommandByName(String cmdName) throws CLIException {
-        switch (cmdName) {
-            case "cat":
-                return new CatCommand();
-            case "wc":
-                return new WcCommand();
-            case "pwd":
-                return new PwdCommand();
-            case "exit":
-                return new ExitCommand();
-            case "echo":
-                return new EchoCommand();
-            default:
-                return new ExecuteCommand(cmdName);
+        if (!STR_TO_CMD_FACTORY.containsKey(cmdName)) {
+            CLIException e = new CLIException("Not known command: " + cmdName);
+            e.setNeedToPrintStackTrace(false);
+            throw e;
         }
+        return STR_TO_CMD_FACTORY.get(cmdName).apply("booba");
+
     }
 }
